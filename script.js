ad6ef21afd777880209062a33b30b079011815fc
Leonardo Nóbrega <leonardo.nobrega56@gmail.com>
2022-12-23 19:37:19 -0300

    New page, new functions (buttons), new styles

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ function toggleMode(){
 html.classList.toggle('light')
 
 }
+
+function showSwiper() {
+  const showSlide = document.getElementById('swiper')
+  if (showSlide.style.display === "none"){
+    showSlide.style.display = "block"
+  } else {
+    showSlide.style.display = "none"
+  }
+}
+
+function showAbout(){
+  const about = document.getElementById('about')
+  if(about.style.display === "none"){
+    about.style.display = "block"
+  } else {
+    about.style.display = "none"
+  }
+}
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 25,
+  freeMode: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
